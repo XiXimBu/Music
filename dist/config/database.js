35c0e08bb86097a677b1016705b25c24dfcaeaf7
XiXimBu <xiximbu@gmail.com>
2026-03-25 17:47:36 +0700

    .

--- dist/config/database.js
+++ dist/config/database.js
@@ -5,39 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connect = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-let cached = global.mongoose;
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
+let isConnected = false;
 const connect = async () => {
+    if (isConnected) {
+        return;
+    }
     const uri = (process.env.MONGOOSE_URL || process.env.MONGO_URI);
     if (!uri) {
         console.error('MongoDB URI is missing!');
         return;
     }
-    if (cached.conn) {
-        return;
-    }
-    if (!cached.promise) {
+    try {
         const options = {
             autoIndex: true,
-            serverSelectionTimeoutMS: 20000,
-            connectTimeoutMS: 20000,
-            socketTimeoutMS: 45000,
+            connectTimeoutMS: 10000,
         };
-        console.log('Đang thiết lập kết nối mới tới MongoDB...');
-        cached.promise = mongoose_1.default.connect(uri, options).then((m) => {
-            console.log('Kết nối MongoDB thành công!');
-            return m;
-        });
-    }
-    try {
-        cached.conn = await cached.promise;
+        await mongoose_1.default.connect(uri, options);
+        isConnected = true;
+        console.log('Connected to MongoDB');
     }
     catch (error) {
-        cached.promise = null;
-        console.error('Lỗi kết nối MongoDB:', error);
-        throw error;
+        console.error('Error connecting to MongoDB:', error);
     }
 };
 exports.connect = connect;
